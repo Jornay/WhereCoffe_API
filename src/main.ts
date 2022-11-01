0448ac076e341d69e3ba40516206daa3bc00ceaf
Jornay <lucasdeafg@yahoo.com.br>
2022-11-01 19:51:24 -0300

    add.procfile-heroku

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { environment } from './environment/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+  });
+  
   await app.listen(environment.PORT || 3000);
 }
 bootstrap();
